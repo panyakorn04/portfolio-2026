6c7b9244d6e9089010a0597c6eca78d3fe72ded1
Panyakorn Boonyong <panyakorn40@gmail.com>
2026-07-02 12:39:56 +0700

    :bug: ..

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -8,26 +8,28 @@ import { PortfolioShell } from "../_components/portfolio-shell";
 import { hasLocale } from "../_data/portfolio";
 import { getDictionary } from "./dictionaries";
 
-export default async function LocalizedHomePage({ params }: PageProps<"/[lang]">) {
-  const { lang } = await params;
+export default async function LocalizedHomePage({
+    params,
+}: PageProps<"/[lang]">) {
+    const { lang } = await params;
 
-  if (!hasLocale(lang)) {
-    notFound();
-  }
+    if (!hasLocale(lang)) {
+        notFound();
+    }
 
-  await connection();
+    await connection();
 
-  const [dictionary, articles] = await Promise.all([
-    getDictionary(lang),
-    listArticles(lang, 4),
-  ]);
+    const [dictionary, articles] = await Promise.all([
+        getDictionary(lang),
+        listArticles(lang, 4),
+    ]);
 
-  return (
-    <PortfolioShell
-      locale={lang}
-      dictionary={dictionary}
-      articles={articles}
-      appVersion={packageJson.version}
-    />
-  );
+    return (
+        <PortfolioShell
+            locale={lang}
+            dictionary={dictionary}
+            articles={articles}
+            appVersion={packageJson.version}
+        />
+    );
 }
